2d7e304e6501847c682bc98e85421089df328bcf
JukioNT <jjulio2468@gmail.com>
2022-07-14 20:21:21 -0300

    React

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,4 @@
-import { createGlobalStyle } from "styled-components"
+export { default as LayoutPage } from "./Layout";
+export { default as HomePage } from "./Home";
+export { default as CadastrarPage } from "./Cadastrar";
+export { default as LoginPage } from "./Login";
